a0772466a4b4d89e4291e26bf8e71298c9f00c64
Yuri <71910210+YurInkognito@users.noreply.github.com>
2025-02-08 23:31:17 -0300

    Bug fix and new script

---
+++ Assets/Scripts/MenuPrincipalManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipalManager :MonoBehaviour
+{
+
+    [SerializeField] private string nomeDoLevelDeJogo;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOpcoes;
+
+    public void Jogar() 
+    {
+        SceneManager.LoadScene(nomeDoLevelDeJogo);
+    }
+
+    public void AbrirOpcoes()
+    {
+        painelMenuInicial.SetActive(false);
+        painelOpcoes.SetActive(true);
+    }
+
+    public void FecharOpcoes()
+    {
+        painelMenuInicial.SetActive(true);
+        painelOpcoes.SetActive(false);
+    }
+
+    public void SairJogo()
+    {
+        Debug.Log("Sair do Jogo");
+        Application.Quit();
+    }
+}
--- Assets/Scripts/TriggerDamage.cs
+++ Assets/Scripts/TriggerDamage.cs
@@ -2,18 +2,34 @@ using UnityEngine;
 
 public class Barrier : MonoBehaviour
 {
-    public int dano = 1; // Quantidade de dano que a barreira causa ao ser atingida
+    public int dano = 1; 
+    public Vector2 respawnPoint = Vector2.zero; 
 
-    [System.Obsolete]
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Bola"))
         {
+        
             HeartSystem playerLife = FindObjectOfType<HeartSystem>();
-
             if (playerLife != null)
             {
-                playerLife.vida -= dano; // Reduz a vida do player
+                playerLife.vida -= dano;
+            }
+
+            Ball ballScript = collision.gameObject.GetComponent<Ball>();
+
+            if (ballScript != null)
+            {
+                Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+                if (rb != null)
+                {
+                    rb.linearVelocity = Vector2.zero; 
+                    rb.angularVelocity = 0f; 
+                    collision.gameObject.transform.position = respawnPoint; 
+                   
+                    Vector2 direcao = new Vector2(1, 1).normalized; 
+                    rb.linearVelocity = direcao * ballScript.velocidadeDaBola;
+                }
             }
         }
     }
