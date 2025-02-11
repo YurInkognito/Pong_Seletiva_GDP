f608e61a5fab19403c48798a3235b2eb16e61513
Yuri <71910210+YurInkognito@users.noreply.github.com>
2025-02-11 01:52:57 -0300

    add scirpts

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,3 +1,4 @@
+using Unity.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -12,9 +13,29 @@ public class Ball : MonoBehaviour
 
     public AudioSource somDaBola;
 
+    public GameObject objetoReset;
+    public float tempoDeContato = 0f;
+    private bool emContato = false;
+
+    private float tempoDeVelocidade = 0f;
+    private float incrementoDeVelocidade = 1f;
+
     void Start()
     {
         MoverBola();
+    }
+
+    void Update()
+    {
+        tempoDeVelocidade += Time.deltaTime;
+
+        if (tempoDeVelocidade >= 3f)
+        {
+            velocidadeDaBola += incrementoDeVelocidade;
+            tempoDeVelocidade = 0f;
+            Debug.Log("Velocidade aumentada");
+            //MoverBola();
+        }
     }
     private void MoverBola()
     {
@@ -23,7 +44,38 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collisionInfo)
     {
+        if(collisionInfo.gameObject == objetoReset)
+        {
+            emContato = true;
+        }
+
         somDaBola.Play();
         oRigidbody2D.linearVelocity += new Vector2(direcaoAleatoriaX, direcaoAleatoriaY);
+
+        if (emContato && collisionInfo.gameObject == objetoReset)
+        {
+            tempoDeContato += Time.deltaTime;
+
+            if (tempoDeContato >= 2f)
+            {
+                ResetarBola();
+                tempoDeContato = 0f;
+            }
+        }
+
+        if (collisionInfo.gameObject == objetoReset)
+        {
+            emContato = true;
+            tempoDeContato = 0f;
+        }
     }
+
+    private void ResetarBola()
+    {
+        Debug.Log("Bola resetada ");
+        transform.position = Vector2.zero;
+        oRigidbody2D.linearVelocity = Vector2.zero;
+        MoverBola();
+    }
+
 }
